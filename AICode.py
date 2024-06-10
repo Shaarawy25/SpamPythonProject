64858f1bb58bda8c6d7125c3fb8e8f8da71bafbd
Shaarawy25 <149785598+Shaarawy25@users.noreply.github.com>
2024-06-10 22:27:27 +0300

    Update AICode.py

--- AICode.py
+++ AICode.py
@@ -6,34 +6,41 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 
 
+data = pd.read_csv("sms_spam.csv")
 
-data = pd.read_csv('sms_spam.csv')
-x = data['text']
-y = data['type']
+x = data["text"] 
+y = data['type'] 
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=1)
 
-vectorizer = CountVectorizer(ngram_range=(1, 2), stop_words='english').fit(x_train)
+vectorizer = CountVectorizer().fit(x_train)
 sms_train_vectorized = vectorizer.transform(x_train)
 sms_test_vectorized = vectorizer.transform(x_test)
-
 
 clfr = MultinomialNB()
 clfr.fit(sms_train_vectorized, y_train)
 
+
 predicted = clfr.predict(sms_test_vectorized)
-print(metrics.classification_report(y_test, predicted))
+acc = metrics.accuracy_score(y_test, predicted)
+
+
+
 
 st.title("Spam Classification App")
-st.image('SpamIMG.png', use_column_width=True)
-st.text('Model Description: Naive Bayes Model, trained on sms data')
+st.image("spam.jpg", use_column_width=True)
+st.text("Model Description: Naive Bayes Model trained on SMS data")
 
-text = st.text_input("Enter Text Here", "Type Here...")
-predict = st.button('Predict')
+
+
+text = st.text_input("Enter text here", "Type here...")
+predict = st.button("Predict")
+
 if predict:
-    new_test_data = vectorizer.transform([text])
-    predicted_label = clfr.predict(new_test_data)[0]
-    prediction_text = "Spam" if predicted_label == "spam" else "Human"
+    newTestData = vectorizer.transform([text])
+    predicted_label = clfr.predict(newTestData)[0]
+    prediction_text = "Spam" if predicted_label == 'spam' else "Human"
+
     if predicted_label == 'spam':
         st.error(f"'{text}' is classified as {prediction_text}")
     else:
