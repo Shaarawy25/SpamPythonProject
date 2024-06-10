191d6bab1ead6608a6a3a21f36d1430d37ad63af
Shaarawy25 <149785598+Shaarawy25@users.noreply.github.com>
2024-06-10 22:24:51 +0300

    Update AICode.py second time

--- AICode.py
+++ AICode.py
@@ -4,9 +4,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
-from imblearn.over_sampling import SMOTE
 
-# Load data
+
+
 data = pd.read_csv('sms_spam.csv')
 x = data['text']
 y = data['type']
@@ -17,11 +17,9 @@ vectorizer = CountVectorizer(ngram_range=(1, 2), stop_words='english').fit(x_tra
 sms_train_vectorized = vectorizer.transform(x_train)
 sms_test_vectorized = vectorizer.transform(x_test)
 
-sm = SMOTE(random_state=0)
-sms_train_vectorized_resampled, y_train_resampled = sm.fit_resample(sms_train_vectorized, y_train)
 
 clfr = MultinomialNB()
-clfr.fit(sms_train_vectorized_resampled, y_train_resampled)
+clfr.fit(sms_train_vectorized, y_train)
 
 predicted = clfr.predict(sms_test_vectorized)
 print(metrics.classification_report(y_test, predicted))
